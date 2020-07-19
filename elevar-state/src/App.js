2e938f2f6a282788a370533f0d23710d290d0fa6
Tarcisio Neto <tarcisioneto1998@gmail.com>
2020-07-19 13:39:31 -0300

    Adicionado conversor de Celcius para Fahrenheit e vice-versa.

--- elevar-state/src/App.js
+++ elevar-state/src/App.js
@@ -1,28 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function Mostrar(props){
+/*function Mostrar(props){
   return (parseFloat(props.temperatura) >= 100) ? <p>A água irá ferver!</p> : <p>A água não irá ferver!</p>;
+}*/
+
+const scales = {c:'Celcius', f:'Fahrenheit'};
+
+class MostrarEntradas extends React.Component{
+
+  constructor(props){
+    super(props);
+    this.alterarTemperatura = this.alterarTemperatura.bind(this);
+  }
+
+  alterarTemperatura(event){
+    this.props.mudarTemperatura(event.target.value, this.props.scale);
+  }
+
+  render(){
+    return (
+      <fieldset>
+        <legend>Digite a temperatura em {scales[this.props.scale]}</legend>
+        <input value = {this.props.temperatura} type="number" onChange={this.alterarTemperatura}/>
+      </fieldset>
+    );  
+  }
+}
+
+function paraCelcius(valor){
+  return (parseFloat(valor) - 32) * (5/9);
+}
+
+function paraFahrenheit(valor){
+  return (parseFloat(valor) * 9/5) + 32;
 }
 
 class Calculadora extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = {temp: '100'};
-    this.alterarTemp = this.alterarTemp.bind(this);
+    this.state = {temp: '', escala:'c'};
+    this.alterarTempCelcius = this.alterarTempCelcius.bind(this);
+    this.alterarTempFahrenheit = this.alterarTempFahrenheit.bind(this);
   }
 
-  alterarTemp(event){
-    this.setState({temp: event.target.value});
+  alterarTempCelcius(temperatura, scale){
+    this.setState({temp: temperatura, escala: scale});
+  }
+
+  alterarTempFahrenheit(temperatura, scale){
+    this.setState({temp: temperatura, escala: scale});
   }
 
   render(){//Quando o state varia, o elemento que o utiliza é renderizado novamente.
+    var tempConvertida = (this.state.escala === 'c') ? paraFahrenheit(this.state.temp) : paraCelcius(this.state.temp);
+    
     return(
       <form>
-        <input type="number" onChange={this.alterarTemp} />
-        <Mostrar temperatura = {this.state.temp}/>
+        <MostrarEntradas temperatura = {this.state.escala === 'f' ? tempConvertida : this.state.temp} 
+        mudarTemperatura={this.alterarTempCelcius} scale={'c'}/>
+        <MostrarEntradas temperatura = {this.state.escala === 'c' ? tempConvertida : this.state.temp} 
+        mudarTemperatura={this.alterarTempFahrenheit} scale={'f'}/>
       </form>
     );
   }
